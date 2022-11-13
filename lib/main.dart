5acaf48fc4c6e86b9585b0621182aeceecc55042
omidkarami77 <omidtkd77@gmail.com>
2022-11-13 11:40:55 +0330

    item list created

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(child: BannerSlider()),
+        body: SafeArea(
+            child: Center(
+                child: Column(
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  height: 56,
+                  width: 56,
+                  decoration: ShapeDecoration(
+                      color: Colors.red,
+                      shadows: [
+                        BoxShadow(
+                          color: Colors.red,
+                          blurRadius: 60,
+                          spreadRadius: -6,
+                          offset: Offset(0.0, 15),
+                        ),
+                      ],
+                      shape: ContinuousRectangleBorder(
+                          borderRadius: BorderRadius.circular(40))),
+                ),
+                Icon(
+                  Icons.mouse,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("hello")
+          ],
+        ))),
       ),
     );
   }
